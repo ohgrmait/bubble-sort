01b8532ef947fe0bc94d224615a3b9ca036262a8
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-12 00:30:16 +0530

    Optimize bubble sort to allow 50% improvement
    Causes a 50% improvement in the worst-case scenario because it reassigns
    length such that the several positions that were already sorted don't
    have to get looked at again.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,21 +2,22 @@ def bubble_sort(array)
   length = array.length
 
   loop do
-    swapped = false
-
+    new_length = 0
+    
     array.each_with_index do |num, idx|
-      next if idx < 1 || idx > length
-
+      next if idx < 1 || idx > length - 1
       if array[idx - 1] > array[idx]
         array[idx - 1], array[idx] = array[idx], array[idx - 1]
-        swapped = true
+        new_length = idx
       end
     end
-
-    length -= 1
-
-    break if not swapped
+    
+    length = new_length
+    
+    break if length <= 1
   end
 
   array
 end
+
+p bubble_sort([4,3,78,2,0,2])
