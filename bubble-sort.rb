9f7f42b146f2e9284be2ea92f843d393364ca72d
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-12 00:24:52 +0530

    Optimize bubble sort to take care of final positions

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,15 +1,19 @@
 def bubble_sort(array)
+  length = array.length
+
   loop do
     swapped = false
 
     array.each_with_index do |num, idx|
-      next if idx == 0 || idx == array.length
+      next if idx < 1 || idx > length
 
       if array[idx - 1] > array[idx]
         array[idx - 1], array[idx] = array[idx], array[idx - 1]
         swapped = true
       end
     end
+
+    length -= 1
 
     break if not swapped
   end
